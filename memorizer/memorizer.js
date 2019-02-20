0a2fa743bb7f7148f891bd3233536a0f154b145a
cutty853 <cutty853@gmail.com>
2019-02-20 17:56:08 +0100

    Added some basic documentation on the memorizer class

--- memorizer/memorizer.js
+++ memorizer/memorizer.js
@@ -17,7 +17,16 @@ var md = new MarkdownIt({
   }
 });
 
+/**
+ * The Memorizer is here to manage a database of memos.
+ */
 class Memorizer {
+  /**
+   * 
+   * @param {String} category The category of the memos
+   * @param {options} configuration A javascript object with the following attributes:
+   *        directory: the subfolder where to put all the memos and their images
+   */
   constructor (category, configuration) {
     this.category = category;
     this.config = configuration;
@@ -39,15 +48,32 @@ class Memorizer {
     return this._category;
   }
 
+  /**
+   * Get the absolute path of a memo.
+   * 
+   * @param {String} memoName The name of the memo you want to get the full path.
+   */
   memoPath(memoName) {
     return path.join(__dirname, this.config.directory, memoName + '.md');
   }
 
+  /**
+   * Get the absolute path of the image of a memo.
+   * 
+   * @param {String} memoName The name of the memo you want to get the image's full path
+   */
   imagePath (memoName) {
     return path.join(__dirname, this.config.directory, 'images', memoName + '.jpg');
   }
 
+  /**
+   * Get the HTML content of a memo with its name.
+   * 
+   * @param {String} memoName The name of a memo
+   * @param {function} treatmentCallback A callback function to handle the html content of the memo.
+   */
   show(memoName, treatmentCallback) {
+    // TODO: Put error handling outside of the Memorizer object.
     fs.readFile(this.memoPath(memoName), {encoding: 'utf-8'}, function (err, content) {
       if (err) {
         console.log("error while loading memo called " + memoName);
@@ -59,6 +85,11 @@ class Memorizer {
     });
   }
 
+  /**
+   * Get the list of all memos managed by the Memorizer.
+   * 
+   * @param {function} treatmentCallback A callback function to handle the list of memos.
+   */
   listAll(treatmentCallback) {
     fs.readdir(path.join(__dirname, this.config.directory), {withFileTypes: true}, function(err, files) {
       var markdownFiles = [];
